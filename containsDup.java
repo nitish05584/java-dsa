b5c22134a5a6e87cc53c2489625d92c180ff8c07
Nitish <devnath8471@gmail.com>
2026-02-10 16:16:38 +0530

    contains

---
+++ containsDup.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+ public class containsDup {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]!=nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
