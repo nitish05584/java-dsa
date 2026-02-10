711be437b387e537e583e292c4bbd9ba67586f90
Nitish <devnath8471@gmail.com>
2026-02-10 16:44:53 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+public class palindrome {
+    public static boolean isPalindrome(String str){
+        int n=str.length();
+        for(int i=0; i<n; i++){
+        if(str.charAt(i)!=str.charAt(n-i-1)){
+            return false;
+        }
+        }
+        return true;
+    }
+   public static void main(String[] args) {
+    String str="racecar";
+   
+    System.out.println( isPalindrome(str));
+   } 
+}
+
+
+
+
+
+
