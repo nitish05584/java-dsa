5ffa049ef8b31e0dae1ea835d30e398c53411b15
Nitish <devnath8471@gmail.com>
2026-02-13 16:29:46 +0530

    oddeven

---
+++ binaryoddeven.java
@@ -0,0 +1,15 @@
+public class binaryoddeven {
+    public static void oddOrEven(int n){
+         int bitmask=1;
+        if((n&bitmask)==0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String[] args) {
+       oddOrEven(3);
+       oddOrEven(11);
+       oddOrEven(14);
+    }
+}
