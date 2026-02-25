25e62b7e78f45e299b86f05f58c0474570072cc1
Nitish <devnath8471@gmail.com>
2026-02-25 10:01:09 +0530

    set

---
+++ setarraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class setarraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+         list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.set(2,5));
+        System.out.println(list);
+    }
+}
