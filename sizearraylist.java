de8da27025d57410c00fe94af0ba938721b11bb3
Nitish <devnath8471@gmail.com>
2026-02-25 10:13:42 +0530

    size

---
+++ sizearraylist.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ public class sizearraylist {
+   public static void main(String[] args) {
+    ArrayList<Integer>list=new ArrayList<>();
+    
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.size());
+   } 
+}
