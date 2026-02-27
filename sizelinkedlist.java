3b8117e46c363c7ad86e451ed6fb1af83a2e94d3
Nitish <devnath8471@gmail.com>
2026-02-27 13:10:34 +0530

    commit

---
+++ sizelinkedlist.java
@@ -0,0 +1,64 @@
+public class sizelinkedlist {
+    public static class Node{
+        int data;
+        Node next;
+    
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+   public static Node head;
+   public static Node tail;
+   public static int size;
+   public static void addFirst(int data){
+    Node newNode=new Node(data);
+    size++;
+    if(head==null){
+    head=tail=newNode;
+    return;
+    }
+    newNode.next=head;
+    head=newNode;
+   }
+   public static void addLast(int data){
+    Node newNode=new Node(data);
+    size++;
+    if(head==null){
+        head=tail=newNode;
+        return;
+    }
+    tail.next=newNode;
+    tail=newNode;
+   }
+   public void print(){
+   if(head==null){
+      System.out.println("ll is empty");
+   }
+   Node temp=head;
+   while(temp!=null){
+      System.out.print(temp.data+"->");
+      temp=temp.next;
+   }
+   System.out.println("null");
+
+ }
+    public static void main(String[] args) {
+       sizelinkedlist ll=new sizelinkedlist();
+       ll.addFirst(2);
+
+    ll.addFirst(1);
+
+    ll.addLast(3);
+
+    ll.addLast(4);
+  
+     
+      ll.print(); 
+      
+      System.out.println(ll.size);
+
+
+
+    }
+}
