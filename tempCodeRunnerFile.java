48abeced1f59ac3182ae88a035dfa013c50cd9d2
Nitish <devnath8471@gmail.com>
2026-02-28 15:05:46 +0530

    detecting

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,11 +1,1 @@
-class Calculator{
-//     int sum(int a,int b){
-//         return a+b;
-//     }
-//     float sum(float a,float b){
-//         return a+b;
-//     }
-//      int sum(int a,int b,int c){
-//         return a+b+c;
-//     }
-// }
+    head.next.next.next=head;
