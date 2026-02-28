a64b3cfba010cfef8488b256f2c472b46e8b634d
Nitish <devnath8471@gmail.com>
2026-02-28 13:36:44 +0530

    remove

---
+++ removenthnode.java
@@ -0,0 +1,89 @@
+public class removenthnode {
+   
+      
+      public static class Node{
+        int data;
+        Node next;
+    
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public static Node head;
+   public static Node tail;
+   public static int size;
+   public static void addFirst(int data){
+    Node newNode=new Node(data);
+    size++;
+    if(head==null){
+    head=tail=newNode;
+    return;
+    }
+    newNode.next=head;
+    head=newNode;
+   }
+    public static void addLast(int data){
+    Node newNode=new Node(data);
+    size++;
+    if(head==null){
+        head=tail=newNode;
+        return;
+    }
+    tail.next=newNode;
+    tail=newNode;
+   }
+
+
+   public void deleteNthformEnd(int n){
+    int sz=0;
+    Node temp=head;
+    while(temp!=null){
+       temp=temp.next;
+       sz++;
+    }
+    if(n==sz){
+        head=head.next;
+    }
+    int i=1;
+    int iToFind=sz-n;
+    Node prev=head;
+    while(i<iToFind){
+    prev=prev.next;
+    i++;
+    }
+    prev.next=prev.next.next;
+    return; 
+   }
+  
+  
+  
+ public void print(){
+   if(head==null){
+      System.out.println("ll is empty");
+   }
+   Node temp=head;
+   while(temp!=null){
+      System.out.print(temp.data+"->");
+      temp=temp.next;
+   }
+   System.out.println("null");
+
+ }
+    public static void main(String[] args) {
+        removenthnode ll=new removenthnode();
+    ll.addFirst(2);
+
+    ll.addFirst(1);
+
+    ll.addLast(3);
+
+    ll.addLast(4);
+
+        ll.print(); 
+
+     ll.deleteNthformEnd(3);
+        ll.print();
+   } 
+}
+
