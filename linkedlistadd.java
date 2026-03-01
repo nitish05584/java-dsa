1e5ac45909bdfc2c8464c739d54472673764344e
Nitish <devnath8471@gmail.com>
2026-03-01 13:02:48 +0530

    linked

---
+++ linkedlistadd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class linkedlistadd {
+   public static void main(String[] args) {
+    LinkedList<Integer>ll=new LinkedList<>();
+    ll.addFirst(1);
+    ll.addFirst(2);
+    ll.addFirst(0);
+    System.out.println(ll);
+   } 
+}
