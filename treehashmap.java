36f9c690fe81b5b5c511e581852609df926fa566
Nitish <devnath8471@gmail.com>
2026-03-18 13:59:21 +0530

    treemap

---
+++ treehashmap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class treehashmap {
+
+    public static void main(String[] args) {
+        TreeMap<String,Integer> tm=new TreeMap<>();
+        tm.put("India",100);
+        tm.put("China",150);
+        tm.put("Us",50);
+
+        System.out.println(tm);
+    }
+    
+}
