00f0a6e70083ac24d90703c06c2fa14fdac673f3
Nitish <devnath8471@gmail.com>
2026-06-23 11:25:04 +0530

    push

--- palindrome.java
+++ palindrome.java
@@ -1,7 +1,7 @@
 public class palindrome {
     public static boolean isPalindrome(String str){
         int n=str.length();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n/2; i++){
         if(str.charAt(i)!=str.charAt(n-i-1)){
             return false;
         }
